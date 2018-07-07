35d7d7eca6b54c8dddaaccbc286b740508bf993d
Wu Tang Financial <wutangfinacial@detwa.com>
2018-07-07 10:43:49 -0400

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: detwa_packages
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+package ['tree'] do
+	  action :upgrade
+end
+package ['speedtest-cli'] do
+	  action :upgrade
+end
+package ['mailutils'] do
+	  action :upgrade
+end
+package ['unattended-upgrades'] do
+	  action :upgrade
+end
